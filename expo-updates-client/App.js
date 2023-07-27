a32d478b65a1f13867e84c281ff2bc44f58c79ee
Bavith <iambavith@Baviths-Air.bbrouter>
2023-07-27 06:32:15 +0530

    App update

--- expo-updates-client/App.js
+++ expo-updates-client/App.js
@@ -22,7 +22,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start 3!</Text>
+      <Text>Open up App.js and do somenthing</Text>
       <Text>{Constants.expoConfig.name}</Text>
       <Image source={require('./assets/favicon.png')} />
       <StatusBar style="auto" />
