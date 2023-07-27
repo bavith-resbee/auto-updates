a2aa3af951943a296dc3b1dac23bcea4378f7625
Bavith <iambavith@Baviths-Air.bbrouter>
2023-07-27 06:30:15 +0530

    App update

--- expo-updates-client/App.js
+++ expo-updates-client/App.js
@@ -1,14 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import Constants from 'expo-constants';
 
+import * as Updates from 'expo-updates';
+
 export default function App() {
+
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      alert(update.isAvailable)
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start 3!</Text>
       <Text>{Constants.expoConfig.name}</Text>
       <Image source={require('./assets/favicon.png')} />
       <StatusBar style="auto" />
+      <Button title="Fetch update" onPress={onFetchUpdateAsync} />
     </View>
   );
 }
